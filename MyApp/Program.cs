b4b12eb9064f118ede36b3dc2d09d1265518f060
Teitelman Yehor <teitelman.y.i@nmu.one>
2026-03-14 18:19:13 +0200

    Add GreetUser function to Program.cs

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,9 +1,27 @@
 ﻿using System;
 
 class Program
+
 {
+
     static void Main(string[] args)
+
     {
+
         Console.WriteLine("Hello, World!");
+
+        GreetUser("User");
+
     }
+
+
+
+    static void GreetUser(string name)
+
+    {
+
+        Console.WriteLine($"Hello, {name}!");
+
+    }
+
 }
